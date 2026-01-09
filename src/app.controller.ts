0a97c971e0561ebdbfc5f5d2701b1dbe10c4abc2
zcc <350018736@qq.com>
2026-01-09 23:55:02 +0800

    补充修改

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RequireNoLogin } from './common/decorator/common.decorator';
 
 @Controller()
+@RequireNoLogin()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
